853837178d516a052e82bd6689d0ed85b5523d3d
cyberbullying153-cmyk <cyberbullying153@gmail.com>
2026-07-30 20:39:57 +0530

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,28 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
+
+
+const firebaseConfig = {
+
+  apiKey: "AIzaSyBQukwDZKUaeCheXpDhlgyxYfsr8E7SVhA",
+
+  authDomain: "cybersafe-22ac4.firebaseapp.com",
+
+  projectId: "cybersafe-22ac4",
+
+  storageBucket: "cybersafe-22ac4.firebasestorage.app",
+
+  messagingSenderId: "381504496598",
+
+  appId: "1:381504496598:web:e3dbaa382c13127d4d3a77",
+
+  measurementId: "G-CB1L3GCR2P"
+
+};
+
+
+const app = initializeApp(firebaseConfig);
+
+
+export const db = getFirestore(app);
